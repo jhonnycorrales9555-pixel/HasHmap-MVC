214441993311b0b64849ff5a5d109b80831fc54c
jhonnycorrales9555-pixel <jhonny.corrales9555@utc.edu.ec>
2026-06-18 21:44:39 -0500

    agregar clase ingresar

--- DEBER/src/deber/EJERCICIOHashMap.java
+++ DEBER/src/deber/EJERCICIOHashMap.java
@@ -5,10 +5,32 @@
 package deber;
 import java.util.HashMap;
 import java.util.Scanner;
+
 public class EJERCICIOHashMap {
-HashMap<String,Object>inventario = new HashMap<>();
+    //creacion HashMap para el inventario de una papeleria
+static HashMap<String,Object>inventario = new HashMap<>();
+static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        // TODO code application logic here
+        agregar();
     }
     
+    public static void agregar(){
+        System.out.println("ID: ");
+        String codigo= sc.nextLine();
+        sc.nextLine();
+        System.out.println("NOMBRE: ");
+        String nombre = sc.nextLine();
+        System.out.println("MARCA: ");
+        String marca =sc.nextLine();
+        System.out.println("PRECIO: ");
+        double precio = sc.nextInt();
+        System.out.println("CANTIDAD: ");
+        String cant = sc.nextLine();
+        System.out.println("STOCK ACTUAL: ");
+        String stock = sc.nextLine();
+        
+       String producto= nombre+","+cant+","+stock+",";
+       inventario.put(codigo,producto);
+        
+    }
 }
