84b3f711cd22366ab3c1a38286581e816b62fc88
kevinchingo2458-star <kevin.chingo2458@utc.edu.ec>
2026-06-19 07:36:45 -0500

    correcion

--- DEBER/src/ClienteController/cliente_controller.java
+++ DEBER/src/ClienteController/cliente_controller.java
@@ -9,11 +9,5 @@ package ClienteController;
  * @author pc
  */
 public class cliente_controller {
-    // Atributos
-    private String cedula;
-    private String nombre;
-    private String apellido;
-    private String telefono;
-    private String correo;
     
 }
