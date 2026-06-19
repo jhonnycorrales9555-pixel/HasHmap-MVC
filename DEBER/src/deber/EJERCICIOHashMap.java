82e17b7fe9c0728143251540b9c4117c01bb3dfc
jhonnycorrales9555-pixel <jhonny.corrales9555@utc.edu.ec>
2026-06-18 22:03:21 -0500

    agregar clase mostrar

--- DEBER/src/deber/EJERCICIOHashMap.java
+++ DEBER/src/deber/EJERCICIOHashMap.java
@@ -8,29 +8,50 @@ import java.util.Scanner;
 
 public class EJERCICIOHashMap {
     //creacion HashMap para el inventario de una papeleria
-static HashMap<String,Object>inventario = new HashMap<>();
+static HashMap<String,String>inventario = new HashMap<>();
 static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        agregar();
+        String op=sc.nextLine();
+        switch(op){
+            case "1": ingresar(); break;
+            case "2": mostrar(); break;
+        }
     }
     
-    public static void agregar(){
+    public static void ingresar(){
         System.out.println("ID: ");
-        String codigo= sc.nextLine();
-        sc.nextLine();
+        String codigo = sc.nextLine();
         System.out.println("NOMBRE: ");
         String nombre = sc.nextLine();
         System.out.println("MARCA: ");
         String marca =sc.nextLine();
         System.out.println("PRECIO: ");
-        double precio = sc.nextInt();
+        String precio = sc.nextLine();
         System.out.println("CANTIDAD: ");
         String cant = sc.nextLine();
         System.out.println("STOCK ACTUAL: ");
         String stock = sc.nextLine();
         
-       String producto= nombre+","+cant+","+stock+",";
-       inventario.put(codigo,producto);
-        
+       inventario.put(codigo,nombre+","+cant+","+stock);
+           
+    }
+    public static void mostrar(){
+        if (inventario.isEmpty()) {
+            System.out.println("NO EXISTE");
+            return;
+        }else{
+            for (String codigo :inventario.keySet()) {
+            String [] datos = inventario.get(codigo).split(",");
+                System.out.println("");
+                System.out.println("CODIGO: "+codigo);
+                System.out.println("NOMBRE: "+datos[0]);
+                System.out.println("MARCA: "+datos[1]);
+                System.out.println("PRECIO: "+datos[2]);
+                System.out.println("CANTIDAD: "+datos[3]);
+                System.out.println("STOCK ACTUAL: "+datos[4]);
+            
+                
+            }
+        }
     }
 }
