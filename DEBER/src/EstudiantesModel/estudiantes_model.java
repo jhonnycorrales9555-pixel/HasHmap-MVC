55c85cd722ca1daca602b9fbd64d910d4c07a53e
Nico <nicojorge666@gmail.com>
2026-06-19 08:38:36 -0500

    estudiantesmodel

--- DEBER/src/EstudiantesModel/estudiantes_model.java
+++ DEBER/src/EstudiantesModel/estudiantes_model.java
@@ -9,5 +9,64 @@ package EstudiantesModel;
  * @author Nikholov
  */
 public class estudiantes_model {
+    // Atributos
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private String carrera;
+   
+    
+    // Constructor
+    public estudiantes_model(String cedula, String nombre, String apellido, String telefono, String correo) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.carrera = telefono;
+        
+    }
+        public String getCedula() {
+        return cedula;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public String getCarrera() {
+        return carrera;
+    }
+    public String getCorreo() {
+        return correo;
+    }
+    
+    public void setCedula(String cedula){
+        this.cedula = cedula;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public void setApellido(String apellido){
+        this.apellido = apellido;
+        
+    }
+    public void setTelefono(String telefono){
+        this.telefono = telefono;
+        
+    }
+    public void setCorreo(String correo){
+        this.correo = correo;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return "Cedula: " + cedula +
+               "\nNombre: " + nombre +
+               "\nApellido: " + apellido +
+               "\nTelefono: " + telefono +
+               "\nCorreo: " + correo;
+    }
+
     
 }
