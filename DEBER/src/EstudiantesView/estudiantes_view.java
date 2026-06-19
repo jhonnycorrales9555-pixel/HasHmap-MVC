a86ef6dc3aa7c2f03ae4cf22ddc30e51897e3baa
Nico <nicojorge666@gmail.com>
2026-06-19 08:42:37 -0500

    import packages en view

--- DEBER/src/EstudiantesView/estudiantes_view.java
+++ DEBER/src/EstudiantesView/estudiantes_view.java
@@ -3,6 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package EstudiantesView;
+import EstudiantesController.estudiantes_controller;
+import EstudiantesModel.estudiantes_model;
+import java.util.Scanner;
 
 /**
  *
