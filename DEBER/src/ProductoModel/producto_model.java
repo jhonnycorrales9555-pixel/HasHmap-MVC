cd874fd29c1e6ff3e5daec99c529a0fae05b1f16
wendychasipanta3175 <wendy.chasipanta3175@utc.edu.ec>
2026-06-19 01:44:30 -0500

    constructor

--- DEBER/src/ProductoModel/producto_model.java
+++ DEBER/src/ProductoModel/producto_model.java
@@ -9,10 +9,22 @@ package ProductoModel;
  * @author USER
  */
 public class producto_model {
+    //atributos
     public String codigo;
     public String nombre;
     public String marca;
     public double precio;
     public int cantidad;
     public int stock;
+    
+    //constructor
+    
+    public producto_model(String codigo, String nombre, String marca, double precio, int cantidad, int stock){
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.marca = marca;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.stock = stock;   
+    }
 }
