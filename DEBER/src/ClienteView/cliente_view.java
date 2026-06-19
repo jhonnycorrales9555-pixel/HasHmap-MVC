22f9dcbbbb8e67e85f05a36a1b525736709bd8b2
kevinchingo2458-star <kevin.chingo2458@utc.edu.ec>
2026-06-19 11:02:39 -0500

    td

--- DEBER/src/ClienteView/cliente_view.java
+++ DEBER/src/ClienteView/cliente_view.java
@@ -13,6 +13,66 @@ import java.util.Scanner;
 public class cliente_view {
     Scanner leer = new Scanner(System.in);
     cliente_controller controlador = new cliente_controller();
+
+    public void menu() {
+        while (true) {
+            System.out.println("""
+        ==================================
+              SISTEMA DE CLIENTES
+        ==================================
+        1. Agregar cliente
+        2. Mostrar clientes
+        3. Actualizar cliente
+        4. Eliminar cliente
+        5. Salir
+        ==================================
+        Seleccione una opcion:
+    """);
+            int opc = leer.nextInt();
+            leer.nextLine(); // limpiar buffer
+
+            switch (opc) {
+                case 1:
+                    registrarCliente();
+                    break;
+                case 2:
+                    mostrarClientes();
+                    break;
+                case 3:
+                    System.out.print("Cedula del cliente: ");
+                    String cedula = leer.nextLine();
+                    System.out.print("Nuevo telefono: ");
+                    String telefono = leer.nextLine();
+                    System.out.print("Nuevo correo: ");
+                    String correo = leer.nextLine();
+
+                    boolean telefonoActualizado = controlador.actualizarTelefono(cedula, telefono);
+                    boolean correoActualizado = controlador.actualizarCorreo(cedula, correo);
+
+                    if (telefonoActualizado && correoActualizado) {
+                        System.out.println("Cliente actualizado");
+                    } else {
+                        System.out.println("Cliente no encontrado");
+                    }
+                    break;
+                case 4:
+                    System.out.print("Cedula del cliente: ");
+                    cedula = leer.nextLine();
+                    if (controlador.eliminarCliente(cedula)) {
+                        System.out.println("Cliente eliminado con exito :D");
+                    } else {
+                        System.out.println("No existe cliente a eliminar");
+                    }
+                    break;
+                case 5:
+                    System.out.println("GRACIAS POR USAR EL PROGRAMA");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("opcion incorrecta:D");
+            }
+        }
+    }
     
      public void registrarCliente() {
 
--- DEBER/src/deber/DEBER.java
+++ DEBER/src/deber/DEBER.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package deber;
+import ClienteView.cliente_view;
 import EstudiantesView.estudiantes_view;
+import java.util.Scanner;
 /**
  *
  * @author USUARIO
@@ -14,9 +16,39 @@ public class DEBER {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         estudiantes_view vista =new estudiantes_view();
+        Scanner leer = new Scanner(System.in);
 
-        vista.menu();
+        while (true) {
+            System.out.println("""
+        ==================================
+              MENU PRINCIPAL
+        ==================================
+        1. Sistema de clientes
+        2. Sistema de estudiantes
+        3. Salir
+        ==================================
+        Seleccione una opcion:
+    """);
+            int opc = leer.nextInt();
+            leer.nextLine(); // limpiar buffer
+
+            switch (opc) {
+                case 1:
+                    cliente_view vistaCliente = new cliente_view();
+                    vistaCliente.menu();
+                    break;
+                case 2:
+                    estudiantes_view vistaEstudiantes = new estudiantes_view();
+                    vistaEstudiantes.menu();
+                    break;
+                case 3:
+                    System.out.println("GRACIAS POR USAR EL PROGRAMA");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("opcion incorrecta:D");
+            }
+        }
     
     }
     
