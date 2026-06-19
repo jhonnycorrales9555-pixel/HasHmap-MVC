59c6cb1443f9de1f53efd945a644348e8f8a289f
kevinchingo2458-star <kevin.chingo2458@utc.edu.ec>
2026-06-19 07:34:44 -0500

    Agregar atributos al modelo cliente

--- DEBER/src/ClienteController/cliente_controller.java
+++ DEBER/src/ClienteController/cliente_controller.java
@@ -9,5 +9,11 @@ package ClienteController;
  * @author pc
  */
 public class cliente_controller {
+    // Atributos
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String correo;
     
 }
