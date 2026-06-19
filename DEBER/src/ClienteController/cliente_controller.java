d6de5bff2de84805b080f353abfb616a930288f8
kevinchingo2458-star <kevin.chingo2458@utc.edu.ec>
2026-06-19 08:08:32 -0500

    importando en controller

--- DEBER/src/ClienteController/cliente_controller.java
+++ DEBER/src/ClienteController/cliente_controller.java
@@ -4,10 +4,16 @@
  */
 package ClienteController;
 
+import ClienteModel.cliente_model;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  *
  * @author pc
  */
 public class cliente_controller {
+    private ArrayList<cliente_model> listaClientes;
+    private HashMap<String, cliente_model> mapaClientes;
     
 }
